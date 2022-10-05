2f0d092279ca9b4a65f4e8ced5ddd238ca66fcf1
Shrey Patel <patel.3705@osu.edu>
2022-10-05 00:02:12 -0400

    Initial Commit

---
+++ Pain App/Pain_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pain_AppApp.swift
+//  Pain App
+//
+//  Created by Shrey Patel on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Pain_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
