2579595c715cc6af0aa74e83390329d18df29690
Shrey Patel <patel.3705@osu.edu>
2022-10-07 03:29:43 -0400

    Completed initial setup of amazon amplify by using node. Setup Cocoa Pods for installing packages to the project Integrated amplify into the project using cocoa pods Edited build settings for the Pods project to suppress all the warnings Added code for configuring amplify so that it can actually run when the app starts.

--- Pain App/View/Pain_AppApp.swift
+++ Pain App/View/Pain_AppApp.swift
@@ -6,9 +6,24 @@
 //
 
 import SwiftUI
+import Amplify
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        do {
+            try Amplify.configure()
+            print("Amplify configured")
+        } catch {
+            print("An error occurred setting up Amplify: \(error)")
+        }
+        return true
+    }
+}
 
 @main
 struct Pain_AppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
