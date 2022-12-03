347085e0f386b3d58bf732d13c755ada517f3594
Elijah Yates <elijahyates28@gmail.com>
2022-12-03 17:33:01 -0500

    Adding UI sliders and tick mark draft

--- Pain App/View/PainSlidersView.swift
+++ Pain App/View/PainSlidersView.swift
@@ -7,9 +7,68 @@
 
 import SwiftUI
 
+struct UISliderView: UIViewRepresentable {
+    @Binding var value: Double
+    
+    var minValue = 1.0
+    var maxValue = 100.0
+    var thumbColor: UIColor = .white
+    var minTrackColor: UIColor = .blue
+    var maxTrackColor: UIColor = .lightGray
+    
+    class Coordinator: NSObject {
+        var value: Binding<Double>
+        
+        init(value: Binding<Double>) {
+            self.value = value
+        }
+        
+        @objc func valueChanged(_ sender: UISlider) {
+            self.value.wrappedValue = Double(sender.value)
+        }
+    }
+    
+    func makeCoordinator() -> UISliderView.Coordinator {
+        Coordinator(value: $value)
+    }
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider(frame: .zero)
+        slider.thumbTintColor = thumbColor
+        slider.minimumTrackTintColor = minTrackColor
+        slider.maximumTrackTintColor = maxTrackColor
+        slider.minimumValue = Float(minValue)
+        slider.maximumValue = Float(maxValue)
+        slider.value = Float(value)
+        
+        for i in 1...10 {
+            let tick = UIView(frame: CGRect(x: i * 80, y: (Int(slider.frame.size.height) - 20) / 2, width: 4, height: 50))
+            tick.backgroundColor = UIColor.init(hex: "A8DADC")
+            tick.layer.shadowColor = UIColor.white.cgColor
+            tick.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+            tick.layer.shadowOpacity = 1.0
+            tick.layer.shadowRadius = 0.0
+            tick.tag = 0
+            slider.insertSubview(tick, belowSubview: slider)
+        }
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.valueChanged(_:)),
+            for: .valueChanged
+        )
+        
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.value = Float(value)
+    }
+}
+
 struct PainSlidersView: View {
     @EnvironmentObject var mainViewController: MainViewController
-    @State private var burning: Float = 0.0
+    
+    @State private var burning: Double = 0.0
     @State private var pinching: Float = 0.0
     @State private var shooting: Float = 0.0
     @State private var throbbing: Float = 0.0
@@ -18,6 +77,8 @@ struct PainSlidersView: View {
     @State private var isPinchingEditing = false
     @State private var isShootingEditing = false
     @State private var isThrobbingEditing = false
+    
+    @State private var currentValue = 6.0
     
     var body: some View {
         GeometryReader { geoProxy in
@@ -38,17 +99,21 @@ struct PainSlidersView: View {
                             GeometryReader { geoProxy2 in
                                 VStack {
                                     VStack {
+                                    
                                         HStack {
                                             Spacer()
-                                            Slider(
-                                                value: $burning,
-                                                in: 0...100,
-                                                step: 1,
-                                                onEditingChanged: { editing in
-                                                    isBurningEditing = editing
-                                                }
-                                            )
-                                            .frame(width: geoProxy2.size.width * 0.8, height: geoProxy2.size.height * 0.25)
+                                            UISliderView(value: $burning,
+                                                         minValue: 1.0,
+                                                         maxValue: 100.0,
+                                                         thumbColor: .black,
+                                                         minTrackColor: .red,
+                                                         maxTrackColor: .blue
+                                                         
+                                                        )
+                                            
+                                                            .frame(width: geoProxy2.size.width * 0.8, height: geoProxy2.size.height * 0.25)
+                                            
+                                          //  .frame(width: geoProxy2.size.width * 0.8, height: geoProxy2.size.height * 0.25)
                                             Spacer()
                                             Spacer()
                                             Text("Burning")
@@ -69,6 +134,7 @@ struct PainSlidersView: View {
                                                     isPinchingEditing = editing
                                                 }
                                             )
+                                    
                                             .frame(width: geoProxy2.size.width * 0.8, height: geoProxy2.size.height * 0.25)
                                             Spacer()
                                             Spacer()
